549069200a8f2d42f09473e4d74f174c355f2dcd
Matias <mattepirez@gmail.com>
2024-10-13 18:48:57 -0300

    Preference model and controller with create method.

---
+++ spec/models/preference_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: preferences
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  description :text             not null
+#  restriction :boolean          default(FALSE), not null
+#  user_id     :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_preferences_on_user_id  (user_id)
+#
+require 'rails_helper'
+
+RSpec.describe Preference, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
