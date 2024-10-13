1a0b81e41fe6b3f404ab6c57e6cf21ab8a6c4745
Matias <mattepirez@gmail.com>
2024-10-13 20:18:55 -0300

    Added to controller show method and tests for show.

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
 
 class PreferencesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_preference, only: %i[edit update show destroy]
+
   def index
     @preferences = current_user.preferences
     @pagy, @records = pagy(@preferences)
+  end
+
+  def show; end
+
+  def new
+    @preference = Preference.new
+  end
+
+  def create
+    @preference = current_user.preferences.build(preference_params)
+
+    if @preference.save!
+      redirect_to preference_path(@preference), notice: t('views.preferences.create_success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:name, :description, :restriction)
+  end
+
+  def set_preference
+    @preference = current_user.preferences.find(params[:id])
   end
 end
