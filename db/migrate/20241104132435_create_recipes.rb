d600e8a1823ad0bb7451f8fefb54dc9898032442
Matias Pirez <67205865+CraftedByMatt@users.noreply.github.com>
2024-11-05 17:23:00 -0300

    create recipe with tests. (#4)

---
+++ db/migrate/20241104132435_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.text :ingredients, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
